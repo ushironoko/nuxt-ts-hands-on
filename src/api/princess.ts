f54007e28799feb61a925d0af28f2f8a1cbeb83f
ushironoko <apple19940820@gmail.com>
2019-08-13 17:50:57 +0900

    カード情報取得APIとコンポーネントの準備

---
+++ src/api/princess.ts
@@ -0,0 +1,18 @@
+import { Cards } from 'princess'
+import axiosBase from 'axios'
+const axios = axiosBase.create({
+  baseURL: 'https://api.matsurihi.me/mltd/v1/'
+})
+
+export const getCardData = async (id: number | string) => {
+  const response = await axios.get(`/cards/${id}`)
+  const cards: Cards = response.data
+  return cards
+}
+
+export const getCardList = async () => {
+  const response = await axios.get('/cards/')
+  const cards: Cards = response.data
+  return cards
+}
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,45 @@
 <template>
-  <div class="min-h-screen flex justify-center content-center text-center mx-auto">
-    <page-header title="勉強会"></page-header>
+  <div class="flex justify-center content-center text-center mx-auto">
+    <div>
+      <page-header title="勉強会"></page-header>
+    </div>
+    <div>
+      <card-view></card-view>
+    </div>
+    <div>
+      <input v-model="cardNumber" type="text" placeholder="カード番号を入力" />
+    </div>
+    <div>
+      <button @click="getCardDara">見る</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { Cards } from 'princess'
 import PageHeader from '~/components/PageHeader.vue'
+import CardView from '~/components/CardView.vue'
+import { getCardData } from '~/api/princess'
 
 export default Vue.extend({
   components: {
-    PageHeader
+    PageHeader,
+    CardView
+  },
+  data() {
+    return {
+      cardData: {} as Cards,
+      cardNumber: 0
+    }
+  },
+  methods: {
+    async getCardDara() {
+      const data = await getCardData(this.cardNumber)
+      this.cardData = data
+    }
   }
 })
 </script>
 
-<style>
-</style>
+<style></style>
